915e7907dc6c97d97a6a99a1e26258a94c1ce7f3
snihurenko <snigurenko.anastasiya@gmail.com>
2020-12-22 13:17:35 +0200

    new feat add structure

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    mode: 'development',
+    devServer: {
+        open: true,
+        port: 8080,
+        hot: true,
+        writeToDisk: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env']
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
