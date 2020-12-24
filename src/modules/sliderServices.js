0f8f345c00d52d71ef063de0b8f87ca175d5c8d0
snihurenko <snigurenko.anastasiya@gmail.com>
2020-12-24 10:54:38 +0200

    feat(sliderService.js): new feat

--- src/modules/sliderServices.js
+++ src/modules/sliderServices.js
@@ -1,51 +1,41 @@
+import { EventEmitter } from "events";
+
 const sliderServices = () => {
     const slider = document.querySelector('.services-slider'),
         slide = slider.querySelectorAll('.slide');
 
-    console.log(slide);
+    let currentSlide = 0,
+        slidesToShow = 4;
 
-    // const slidesToShow = 5;
-    // const widthSlide = Math.floor(100/slidesToShow);
-    // slider.style.flex = `0 0 ${widthSlide}% `;
+    slide.forEach(elem => {
+        //elem.style.flex = `0 0 ${Math.floor(100/slidesToShow)}%;`
+        elem.style.flex = '0 0 25%';
+    });
 
-
-    let currentSlide = 0;
-
-    // const prevSlide = (elem, index, strClass) => {
-    //     elem[index].classList.remove(strClass);
-    // };
-    // const nextSlide = (elem, index, strClass) => {
-    //     elem[index].classList.add(strClass);
-    // };
-
-    slider.addEventListener('click', event => {
+    document.addEventListener('click', event => {
         event.preventDefault();
         const target = event.target;
-        console.log(target);
 
         if (!target.matches('.slider-arrow')) {
             return;
         }
 
-        //prevSlide(slide, currentSlide, 'gallery-slide-active');
-
         if (target.matches('#serv-arrow-right')) {
             currentSlide++;
+            if (currentSlide >= slide.length - slidesToShow) {
+                currentSlide = 0;
+            }
             slider.style.transform = `translateX(-${currentSlide * 25}%)`;
-            console.log('right');
         }
+
         if (target.matches('#serv-arrow-left')) {
             currentSlide--;
+            if (currentSlide < 0) {
+                currentSlide = slide.length - slidesToShow;
+            }
+            slider.style.transform = `translateX(-${currentSlide * 25}%)`;
         }
 
-        if (currentSlide >= slide.length) {
-            currentSlide = 0;
-        }
-        if (currentSlide < 0) {
-            currentSlide = slide.length - 1;
-        }
-
-        //nextSlide(slide, currentSlide, 'gallery-slide-active');
     });
 };
 
